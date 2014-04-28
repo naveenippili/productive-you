cede03fb80500bd3211f09285ffa347d32ef1165
Naveen Kumar Ippili <navee1292@gmail.com>
2014-04-28 12:43:21 +0530

    Displaying the current tab url in the popup using the background page. (Not working properly right now. Have to change this logic to use onActivated and onChanged events)

--- js/background.js
+++ js/background.js
@@ -1,3 +1,13 @@
-function getCurrentTime() {
-	return new Date();
+var activeTab = "";
+function getActiveTab() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        console.log("Insdie callback");
+        if (tabs.length < 1) {
+            activeTab = null;
+        } else {
+            activeTab = tabs[0].url;
+        }
+    });
+    console.log("Before returning");
+    return activeTab;
 }
--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-
-    chrome.tabs.query( {}, function (tabs) {
-
-        for (var i = 0; i< tabs.length; i++) {
-            $("#popup-content").append(tabs[i].id + ": ");
-            $("#popup-content").append(tabs[i].url);
-            $("#popup-content").append($("<br/>"));
-        }
-    });
-
-    var curTime = chrome.extension.getBackgroundPage().getCurrentTime();
-    $("#popup-content").append("Current Time: " + curTime);
+    var activeTab = chrome.extension.getBackgroundPage().getActiveTab();
+    $("#popup-content").append("Active Tab: " + activeTab);
 });
